d2ceed01311166a6f9f23ce3eab5a9f1e8ed217a
Marc Wittke <marc@wittke-web.de>
2024-05-17 22:15:45 +0200

    fix warnings

--- src/Backend.Fx.IntegrationEvents.Feature/MessageBus/IMessageBusMessage.cs
+++ src/Backend.Fx.IntegrationEvents.Feature/MessageBus/IMessageBusMessage.cs
@@ -6,11 +6,11 @@ namespace Backend.Fx.IntegrationEvents.Feature.MessageBus;
 [PublicAPI]
 public class MessageBusMessage
 {
-    public Guid Id { get; set; }
+    public required Guid Id { get; set; }
 
-    public Instant CreationDate { get; set; }
+    public required Instant CreationDate { get; set; }
 
-    public Guid CorrelationId { get; set; }
+    public required Guid CorrelationId { get; set; }
 
-    public object Payload { get; set; }
+    public required object Payload { get; set; }
 }
